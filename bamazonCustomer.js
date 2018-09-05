73f2b8c1f354801a0e4a9f0b5915ad6c84fc73ca
cgad <35946778+cgad@users.noreply.github.com>
2018-09-05 01:20:46 -0600

    Function layout

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,51 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+function displayItems() {
+    var divider = "\n\n-------------------------------------------------\n";
+    console.log("\nItems available for sale:" + divider);
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // console.log(res);
+        res.forEach(function(product) {
+            console.log(product.product_name + " (Item ID: " + product.item_id + "), $" + product.price + "\nDepartment: " + product.department_name + divider);
+        });
+        // nesting promptUser() in here allows for correct order of console logging
+        promptUser();
+    });
+    connection.end();
+};
+
+function promptUser() {
+    inquirer.prompt([
+        {
+            message: "Enter the ID of the item you would like to buy:",
+            type: "input",
+            name: "id"
+        },
+        {
+            message: "Enter the number of units you would like to buy:",
+            type: "input",
+            name: "units"
+        }
+    ])
+    .then(function(response) {
+        console.log("hi");
+    })
+}
+
+function checkStock() {
+    // are there enough items in stock to fulfill the user's order?
+    // if enough in stock to fulfill order, update products table with new stock quantity
+    // else, log "Insufficient quantity" & return false
+}
+
+function updateProducts() {
+    // if checkStock(), update products table with new stock quantity
+    // then show customer total cost of their purchase
+}
+
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("hi");
-})
+    displayItems();
+});
