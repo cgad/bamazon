986034cdfa54d42e8645e05f542de90d7e9c5e45
cgad <35946778+cgad@users.noreply.github.com>
2018-09-05 00:22:47 -0600

    Database setup

--- schema.sql
+++ schema.sql
@@ -3,9 +3,12 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-    item_id INT(10) NOT NULL,
+    item_id INT(10) NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(50),
-    price DEC(10,4),
-    stock_quantity INT(10)
+    price DEC(10,2),
+    stock_quantity INT(10),
+    PRIMARY KEY (item_id)
 );
+
+SELECT * FROM products;
---
+++ seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
+("women's snowboard", "outdoor activities", 200, 4),
+("women's snowboard bindings", "outdoor activities", 100, 2),
+("kid's ski & snowboard goggles", "outdoor activities", 50, 2),
+("women's snowboard pants", "outdoor activities", 50, 1),
+("women's snowboard jacket", "outdoor activities", 100, 5),
+("women's snowboard boots", "outdoor activities", 70, 10),
+("women's snowboard socks", "outdoor activities", 20, 10),
+("women's ski & snowboard helmet", "outdoor activities", 50, 2),
+("crash pad", "outdoor activities", 50, 2);
